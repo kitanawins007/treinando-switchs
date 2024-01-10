0dbc4bafb681ed6ce050bf7ddb8e3e3cb1bab749
Lenovo <Lenovo@DESKTOP-J07EOB4>
2024-01-10 11:39:24 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Month
+{
+    class Semana
+    {
+       public static int dia = Convert.ToInt32(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            switch (dia)
+            {
+                case 1: Console.WriteLine("Domingo!");
+                        break;
+                    case 2: Console.WriteLine("Segunda!");
+                        break;
+
+            }
+        }
+    }
+}
+
